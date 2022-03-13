fa1f9c324b7bdf6c146cc9eec453794cb94123ce
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-03-14 02:06:24 +0600

    Finished

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-fetch('https://api.adviceslip.com/advice')
- .then(res => res.json())
- .then(data => generateAdvice(data.slip))
+const fetchAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+    .then(res => res.json())
+    .then(data => generateAdvice(data.slip))
+}
 
  const generateAdvice = data => {
-     document.getElementById('advice-number').innerText = data.id;
+     document.getElementById('advice-number').innerText = data?.id;
      document.getElementById('quote').innerText = `"${data.advice}"`;
  }
 
- generateAdvice()
+ fetchAdvice();
