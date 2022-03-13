918f53109394e5570b67e446f377b8bd28cc8e14
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-03-14 01:34:43 +0600

    Done with main work

---
+++ js/app.js
@@ -0,0 +1,10 @@
+fetch('https://api.adviceslip.com/advice')
+ .then(res => res.json())
+ .then(data => generateAdvice(data.slip))
+
+ const generateAdvice = data => {
+     document.getElementById('advice-number').innerText = data.id;
+     document.getElementById('quote').innerText = `"${data.advice}"`;
+ }
+
+ generateAdvice()
